3a0ca60a320b86d0c93c0d113fb240b53b51df50
Xenopop <83357129+Xenopos@users.noreply.github.com>
2023-02-17 09:33:17 +0800

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,20 @@
+var date;
+var currentbday;
+
+
+
+function Agecheck(){
+
+}
+function PassCheck(){
+    var pass = document.getElementById('pwd');
+
+    if (pass.value.length < 8)
+    {
+        messega.innerfile
+    }
+}
+function ValidationCheck(){
+
+}
+
