6147fdd0515997cb84c3984a3fe35d0f3b1524ef
Xenopop <83357129+Xenopos@users.noreply.github.com>
2023-02-18 21:25:23 +0800

    change 1

--- javas.js
+++ javas.js
@@ -1,4 +1,12 @@
-
+/*Using our Form Activity, create a JavaScript function that:
+Checks if the password is at least 8 characters long. If not, 
+display the error message:
+Password must be at least 8 characters.
+Checks if the age is greater than 21. If not, display the error message:
+Minors are not allowed!
+If the validation is successful, display the success message:
+Hooray! Your account has been successfully created.
+*/
 
 function validateAge() {
 
@@ -9,16 +17,18 @@ function validateAge() {
   var ageInYears = new Date(ageInMs).getFullYear() - 1970;
   
   if (ageInYears >= 18) {
-    alert("You are eligible.");
-  } else {
-    alert("You are not eligible.");
+      
+  } 
+  else {
+    alert("Minors are not allowed!");
   }
+
 function PassCheck(){
     var pass = document.getElementById('pwd');
 
     if (pass.value.length < 8)
     {
-        document.getElementById('message').innerHTML = "GIT GUD"; 
+        document.getElementById('passnotset').innerHTML = "Password must be at least 8 characters."; 
     }
 }
 function ValidationCheck(){
